0c707e3dc04cfea76573ac4b45f10a816084b63f
brad lumber <codedevbrad@gmail.com>
2021-01-30 13:02:33 +0000

    Updates

---
+++ precache-manifest.29add1b0297c70127b55dc5cb8552351.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "ad872ce0ef2bae5d4dc0",
+    "url": "./static/js/main.e8a8de64.chunk.js"
+  },
+  {
+    "revision": "afa75934407fa7ba5477",
+    "url": "./static/js/2.fa25b335.chunk.js"
+  },
+  {
+    "revision": "ad872ce0ef2bae5d4dc0",
+    "url": "./static/css/main.4551966f.chunk.css"
+  },
+  {
+    "revision": "20faebcdd59e40f224f2811487b3fd5d",
+    "url": "./index.html"
+  }
+];
